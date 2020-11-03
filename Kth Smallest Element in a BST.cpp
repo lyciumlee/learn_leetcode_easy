b28a77bf6f5f772c9b2157bb57ba8afe5b14987c
breakingdevil <hkcoldmoon@gmail.com>
2020-11-03 08:54:04 +0800

    10

---
+++ Kth Smallest Element in a BST.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k)
+    {
+        return kthSmallestDFS(root, k);
+    }
+    int kthSmallestDFS(TreeNode* root, int &k)
+    {
+        if(root == nullptr) return -1;
+        int val = kthSmallestDFS(root->left, k);
+        if(k == 0) return val;
+        if(--k == 0) return root->val;
+        return kthSmallestDFS(root->right, k);
+    }
+};
---
+++ Search a 2D Matrix II.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target)
+    {
+        if(matrix.empty() || matrix[0].empty()) return false;
+        if(target < matrix[0][0] || target > matrix.back().back()) return false;
+        int x = matrix.size() - 1, y = 0;
+        while(true)
+        {
+            if(matrix[x][y] > target) x--;
+            else if(matrix[x][y] < target) y++;
+            else return true;
+            if(x < 0 || y >= matrix[0].size()) break;
+        }
+        return false;
+    }
+};
